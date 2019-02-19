8f08742d04fb068d3d7ce4c840f57ed51b65c9d8
ponbac <ponbac@student.chalmers.se>
2019-02-19 01:36:03 +0100

    removed unused tesseract.py, not used

--- tesseract.py
+++
@@ -1,9 +0,0 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-import pytesseract
-
-config = '-l eng --oem 1 --psm 3'
-
-print(pytesseract.image_to_string(Image.open('ship.png'), config=config))
